20d14162c4c0ce554613699554e5e8b5f9d420f2
blackdragon <juliorodriguez2498@gmail.com>
2025-11-14 22:51:11 -0600

    ci: agregamos el componente b al proyecto del examen final

---
+++ ms-proveedores-facturas/src/main/java/com/multipedidos/ms_proveedores_facturas/MsProveedoresFacturasApplication.java
@@ -0,0 +1,13 @@
+package com.multipedidos.ms_proveedores_facturas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsProveedoresFacturasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsProveedoresFacturasApplication.class, args);
+	}
+
+}
---
+++ ms-proveedores-facturas/src/test/java/com/multipedidos/ms_proveedores_facturas/MsProveedoresFacturasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multipedidos.ms_proveedores_facturas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsProveedoresFacturasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
